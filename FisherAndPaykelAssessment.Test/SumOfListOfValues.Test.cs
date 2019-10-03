d4776214579ad015f02b510459abf5b55c8fd5c6
Loan Burger <loan.burger@blackhawktracking.com>
2019-10-03 19:44:18 +1300

    Added new program with test

---
+++ FisherAndPaykelAssessment.Test/SumOfListOfValues.Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace FisherAndPaykelAssessment.Test
+{
+    public class SumOfListOfValues
+    {		
+		[Fact]	
+		public void SumOfListOfValuesTest()
+		{			
+			Assert.True(true);
+		}
+	}
+}
---
+++ FisherAndPaykelAssessment/SumOfListOfValues.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace FisherAndPaykelAssessment
+{
+	/// <summary>
+	/// See this for sum of digits
+	/// https://stackoverflow.com/questions/478968/sum-of-digits-in-c-sharp
+	/// </summary>
+	public class SumOfListOfValues
+	{
+		/// <summary>
+		/// Given a list of values, sum the digits of each value to create a new value and return the list of new values ordered from lowest to highest.
+		/// </summary>
+		/// <param name="args"></param>
+		static void Main(string[] args)
+		{
+
+
+
+
+		}
+
+		/// <summary>
+		/// MAYBE but probably just use list of ints
+		/// </summary>
+		/// <typeparam name="O"></typeparam>
+		/// <typeparam name="I"></typeparam>
+		/// <param name="listOfDigits"></param>
+		/// <returns></returns>
+		public static O PostListWithEntityReponse<O, I>(List<I> listOfDigits)  where O : class where I : class
+		{
+
+
+
+
+
+
+
+
+
+			return default(O);
+		}
+	}
+}
